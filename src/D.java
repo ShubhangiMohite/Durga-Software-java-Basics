e97a30f6604a596d4fe73c1508b98132cb7f3639
ShubhangiMohite <mohiteshubhangii94@gmail.com>
2023-06-14 19:15:12 +0530

    atmost one public class needs in java program

---
+++ src/D.java
@@ -0,0 +1,31 @@
+class A
+{
+    public static void main(String arg[])
+    {
+        System.out.println("Helloi");
+    }
+}
+
+class B
+{
+    public static void main(String args[])
+    {
+        System.out.println("worlddddd");
+    }
+}
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
+public class D
+{
+    public static void main(String args[])
+    {
+        System.out.println("wowwww");
+    }
+
+}
